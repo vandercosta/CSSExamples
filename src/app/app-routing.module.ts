4344ee4dfdb4303dc9605abcff53d98bce180d7a
Vander <vander.costa@cwi.com.br>
2021-09-20 01:11:59 -0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NeonComponent } from './components/neon/neon.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'neon', pathMatch: 'full' },
+  { path: 'neon', component: NeonComponent },
+  { path: '**', component: NeonComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
