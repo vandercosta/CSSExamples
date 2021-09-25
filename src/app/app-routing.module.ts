922c26f6fa4ee7dd92696cf287cd62b73678edfd
Vander <vander.costa@cwi.com.br>
2021-09-25 17:07:07 -0300

    botão efeito gradiente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ButtonGradientBorderComponent } from './components/button-gradient-border/button-gradient-border.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { NeonComponent } from './components/neon/neon.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'neon', pathMatch: 'full' },
   { path: 'neon', component: NeonComponent },
   { path: 'loader', component: LoaderComponent },
+  { path: 'buttonGradientBorder', component: ButtonGradientBorderComponent },
   { path: '**', component: NeonComponent },
 ];
 
---
+++ src/app/components/button-gradient-border/button-gradient-border.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-gradient-border',
+  templateUrl: './button-gradient-border.component.html',
+  styleUrls: ['./button-gradient-border.component.scss'],
+})
+export class ButtonGradientBorderComponent implements OnInit {
+  ngOnInit(): void {
+    console.log();
+  }
+}
