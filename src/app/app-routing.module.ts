0fbd9c5ca824548e55953c9afa34d8f64bb25cb8
Vander <vander.costa@cwi.com.br>
2021-09-25 01:27:43 -0300

    loader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoaderComponent } from './components/loader/loader.component';
 import { NeonComponent } from './components/neon/neon.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'neon', pathMatch: 'full' },
   { path: 'neon', component: NeonComponent },
+  { path: 'loader', component: LoaderComponent },
   { path: '**', component: NeonComponent },
 ];
 
